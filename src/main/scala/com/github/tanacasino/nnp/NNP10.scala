666f2ed8fb6e2876a17acf49865203cce8475af5
Tomofumi Tanaka <tanacasino@gmail.com>
2014-11-12 08:43:52 +0900

    Add NNP Scala P1->P10

---
+++ src/main/scala/com/github/tanacasino/nnp/NNP10.scala
@@ -0,0 +1,50 @@
+package com.github.tanacasino.nnp
+
+
+trait NNP10 {
+
+  // P01 (*) Find the last element of a list.
+  def last(list: List[Int]): Int = {
+    ???
+  }
+
+  // P02 (*) Find the last but one element of a list.
+  def penultimate(list: List[Int]): Int = {
+    ???
+  }
+
+  def nth(n: Int, list: List[Int]): Int = {
+    ???
+  }
+
+  def length(list: List[Int]): Int = {
+    ???
+  }
+
+  def reverse(list: List[Int]): List[Int] = {
+    ???
+  }
+
+  def isPalindrome(list: List[Int]): Boolean = {
+    ???
+  }
+
+  def flatten(nested: List[Any]): List[Any] = {
+    ???
+  }
+
+  def compress(list: List[Symbol]): List[Symbol] = {
+    ???
+  }
+
+  def pack(list: List[Symbol]): List[List[Symbol]] = {
+    ???
+  }
+
+  def encode(list: List[Symbol]): List[(Int, Symbol)] = {
+    ???
+  }
+
+}
+
+
