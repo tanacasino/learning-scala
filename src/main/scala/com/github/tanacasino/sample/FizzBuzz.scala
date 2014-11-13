0e50ed6062b51ad1e0f6c76197d0474701e3c92e
Kimiya Sato <kimiya.sato@bizreach.co.jp>
2014-11-13 11:43:27 +0900

    Q3

--- src/main/scala/com/github/tanacasino/sample/FizzBuzz.scala
+++ src/main/scala/com/github/tanacasino/sample/FizzBuzz.scala
@@ -2,9 +2,10 @@ package com.github.tanacasino.sample
 
 object FizzBuzz extends App {
 
-  execute
+  execute1
+  execute3
 
-  def execute = {
+  def execute1 = {
     (1.to(100)).foreach { i =>
       ((i%3), (i%5)) match {
         case (0, 0) => println("FizzBuzz")
@@ -13,6 +14,20 @@ object FizzBuzz extends App {
         case (_, _) => println(i.toString)
       }
     }
+  }
+
+  def execute3 = {
+    (1 to 100)
+      .filterNot { i => i % 2 == 0 }
+      .map { i =>
+      ((i%3), (i%5)) match {
+        case (0, 0) => "FizzBuzz"
+        case (0, _) => "Fizz"
+        case (_, 0) => "Buzz"
+        case (_, _) => i.toString
+      }
+    }
+    .foreach { println }
   }
 
 }
