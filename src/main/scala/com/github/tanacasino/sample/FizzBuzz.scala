6eb8f2458605269b78704ee976c8a47d49ec9ad9
yu.watanabe <yu.watanabe@bizreach.co.jp>
2014-11-13 11:36:00 +0900

    2の倍数を除外するやつ

--- src/main/scala/com/github/tanacasino/sample/FizzBuzz.scala
+++ src/main/scala/com/github/tanacasino/sample/FizzBuzz.scala
@@ -7,14 +7,33 @@ object FizzBuzz extends App {
 
   // 3の倍数でfizz　5の倍数でbuzz　3,5両方の倍数はfizbuzz
   println("###")
-  for(i <- 1 to 100) {
-    if (i % 3 == 0 && i % 5 == 0) { print("FizzBuzz ") }
-    else if (i % 3 == 0) { print("Fizz ") }
-    else if (i % 5 == 0) { print("Buzz ") }
-    else { print(i + " ") }
+  for (i <- 1 to 100) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      print("FizzBuzz,")
+    }
+    else if (i % 3 == 0) {
+      print("Fizz,")
+    }
+    else if (i % 5 == 0) {
+      print("Buzz,")
+    }
+    else {
+      print(i + ",")
+    }
   }
 
-  println("###")
+  println("### 2の倍数を除外")
+  (1 to 100).toList.filter(n => n % 2 != 0).map(n =>
+    n match {
+      case n if (n % 15 == 0) => print("FizzBuzz,")
+      case n if (n % 3 == 0) => print("Fizz,")
+      case n if (n % 5 == 0) => print("Buzz,")
+      case _ => print(s"$n,")
+    }
 
-
+    //  {
+    //    case n if (n % 15 == 0) => "FizzBuzz"
+    //    case _  n.toString
+    //    }
+  )
 }
