67f8afccdfd8fe6bcb47ca712504c810c6764bbc
kitagawa <kitagawa@bizreach.co.jp>
2014-11-13 11:36:17 +0900

    - FizzBuzz excluding even num

--- src/main/scala/com/github/tanacasino/sample/FizzBuzz.scala
+++ src/main/scala/com/github/tanacasino/sample/FizzBuzz.scala
@@ -7,16 +7,29 @@ import scala.collection.immutable.Range.Inclusive
  */
 object FizzBuzz extends App {
 
-  val inclusive: Inclusive = 1 to 100
-  val list: List[Int] = inclusive.toList
-  fizzBuzzForeach(list)
+  // First
+//  val inclusive: Inclusive = 1 to 100
+//  val list: List[Int] = inclusive.toList
+//  fizzBuzzForeach(list)
+//
+//  def fizzBuzzForeach(list: Seq[Int]) :Unit = {
+//    list.foreach {
+//      case num if num % 3 == 0 && num % 5 == 0 => println("FizzBuzz")
+//      case num if num % 3 == 0 => println("Fizz")
+//      case num if num % 5 == 0 => println("Buzz")
+//      case num => println(num)
+//    }
+//  }
 
-  def fizzBuzzForeach(list: Seq[Int]) :Unit = {
-    list.foreach {
-      case num if num % 3 == 0 && num % 5 == 0 => println("FizzBuzz")
-      case num if num % 3 == 0 => println("Fizz")
-      case num if num % 5 == 0 => println("Buzz")
-      case num => println(num)
-    }
-  }
+  // Exclude 2
+  (1 to 100)
+    .filterNot(_ % 2 == 0)
+    .map{
+    case num if num % 15 == 0 => "FizzBuzz"
+    case num if num % 3 == 0 => "Fizz"
+    case num if num % 5 == 0 => "Buzz"
+    case num => num.toString}
+    .foreach(println(_))
+
+
 }
