aeae18f34903581bc5afbfbead4e8ba1cbde3cc5
Kimiya Sato <kimiya.sato@bizreach.co.jp>
2014-11-14 10:24:31 +0900

    Q4_2

--- src/main/scala/com/github/tanacasino/sample/FizzBuzz.scala
+++ src/main/scala/com/github/tanacasino/sample/FizzBuzz.scala
@@ -34,17 +34,16 @@ object FizzBuzz extends App {
   }
 
   def execute4 = {
-    val join = (1 to 100)
-      .map { i =>
+    val listFizzBuzz:(Int) => (String) = {
+      i =>
         ((i % 3), (i % 5)) match {
           case (0, 0) => "FizzBuzz"
           case (0, _) => "Fizz"
           case (_, 0) => "Buzz"
           case (_, _) => i.toString
         }
-      }
-      .mkString(",")
-    println(join)
+    }
+    println((1 to 100).map(listFizzBuzz).mkString(","))
   }
 
 }
