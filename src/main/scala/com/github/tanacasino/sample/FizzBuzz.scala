48277b96a4e5857ea1aaffb693fd66353777879d
shintaro.tamaki <shintaro.tamaki@bizreach.co.jp>
2014-11-14 09:20:15 +0900

    FizzBuzz その５までやりました～

--- src/main/scala/com/github/tanacasino/sample/FizzBuzz.scala
+++ src/main/scala/com/github/tanacasino/sample/FizzBuzz.scala
@@ -6,13 +6,48 @@ package com.github.tanacasino.sample
 class FizzBuzz {
 
   def FizzBuzz(): Seq[String] = {
-    (1 to 100).map( i => {
-      (i % 3, i % 5) match {
-        case (0, 0) => "FizzBuzz"
-        case (0, _) => "Fizz"
-        case (_, 0) => "Buzz"
-        case _ => i.toString
-      }
-    })
+    (1 to 100)
+      .map( i => {
+        (i % 3, i % 5) match {
+          case (0, 0) => "FizzBuzz"
+          case (0, _) => "Fizz"
+          case (_, 0) => "Buzz"
+          case _ => i.toString
+        }
+      })
   }
+
+  def FizzBuzz2(): Seq[String] = {
+    // 1行で書く
+    (1 to 100).map( i => { if (i % 3 == 0) "Fizz" else i.toString})
+  }
+
+  def FizzBuzz3(): Seq[String] = {
+    // 2の倍数は除外
+    (1 to 100)
+      .map( i => {
+        (i % 3, i % 5, i % 2) match {
+          case (_, _, 0) => None
+          case (0, 0, _) => Some("FizzBuzz")
+          case (0, _, _) => Some("Fizz")
+          case (_, 0, _) => Some("Buzz")
+          case _ => Some(i.toString)
+        }
+      })
+      .flatten
+  }
+
+  def FizzBuzz4(): String = {
+    FizzBuzz()
+      .mkString(",")
+  }
+
+  def FizzBuzz5(): Long = {
+    val list: Seq[String] = FizzBuzz()
+    list
+      .filter (_.matches("""\d+"""))
+      .sum
+      .toLong
+  }
+
 }
--- src/test/scala/com/github/tanacasino/sample/FizzBuzzSpec.scala
+++ src/test/scala/com/github/tanacasino/sample/FizzBuzzSpec.scala
@@ -4,11 +4,30 @@ import org.scalatest._
 
 class FizzBuzzSpec extends FlatSpec with Matchers {
 
-  "A FizzBuzz" should " " in {
+  "A FizzBuzz" should "A FizzBuzz " in {
+    println("================ FizzBuzz")
     val target = new FizzBuzz
-    target.FizzBuzz().map(
-      println(_)
-    )
+    target.FizzBuzz().foreach(println)
   }
+
+  "A FizzBuzz3" should "A FizzBuzz3 " in {
+    println("================ FizzBuzz3")
+    val target = new FizzBuzz
+    target.FizzBuzz3().foreach(println)
+  }
+
+  "A FizzBuzz4" should "A FizzBuzz4 " in {
+    println("================ FizzBuzz4")
+    val target = new FizzBuzz
+    println(target.FizzBuzz4())
+  }
+
+  "A FizzBuzz5" should "A FizzBuzz5 " in {
+    println("================ FizzBuzz5")
+    val target = new FizzBuzz
+    val ret = target.FizzBuzz5()
+    println(s"sum = ${ret}")
+  }
+
 }
 
