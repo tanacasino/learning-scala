27835370ff3028da2dbf50bb760eac594232ab10
kitagawa <kitagawa@bizreach.co.jp>
2014-11-14 10:00:17 +0900

    - FizzBuzz up to version 5

--- src/main/scala/com/github/tanacasino/sample/FizzBuzz.scala
+++ src/main/scala/com/github/tanacasino/sample/FizzBuzz.scala
@@ -1,35 +1,75 @@
 package com.github.tanacasino.sample
 
 import scala.collection.immutable.Range.Inclusive
+import scala.util.Try
 
 /**
  * Created by kitagawa on 2014/11/12.
  */
 object FizzBuzz extends App {
 
+  fizzBuzzNumSum
+
   // First
-//  val inclusive: Inclusive = 1 to 100
-//  val list: List[Int] = inclusive.toList
-//  fizzBuzzForeach(list)
-//
-//  def fizzBuzzForeach(list: Seq[Int]) :Unit = {
-//    list.foreach {
-//      case num if num % 3 == 0 && num % 5 == 0 => println("FizzBuzz")
-//      case num if num % 3 == 0 => println("Fizz")
-//      case num if num % 5 == 0 => println("Buzz")
-//      case num => println(num)
-//    }
-//  }
+  def fizzBuzzFirst() = {
+    val inclusive: Inclusive = 1 to 100
+    val list: List[Int] = inclusive.toList
+    fizzBuzzForeach(list)
 
-  // Exclude 2
-  (1 to 100)
-    .filterNot(_ % 2 == 0)
-    .map{
-    case num if num % 15 == 0 => "FizzBuzz"
-    case num if num % 3 == 0 => "Fizz"
-    case num if num % 5 == 0 => "Buzz"
-    case num => num.toString}
-    .foreach(println(_))
+    def fizzBuzzForeach(list: Seq[Int]) :Unit = {
+      list.foreach {
+        case num if num % 3 == 0 && num % 5 == 0 => println("FizzBuzz")
+        case num if num % 3 == 0 => println("Fizz")
+        case num if num % 5 == 0 => println("Buzz")
+        case num => println(num)
+      }
+    }
+  }
 
+  // Exclude Even Num
+  def fizzBuzzExcludeEvenNum = {
+    println {
+      (1 to 100)
+        .filter(_ % 2 == 0)
+        .map {
+        case num if num % 15 == 0 => "FizzBuzz"
+        case num if num % 3 == 0 => "Fizz"
+        case num if num % 5 == 0 => "Buzz"
+        case num => num.toString
+      }
+      .foreach(println)
+    }
+  }
 
+  // Comma Separated
+  def fizzBuzzCommaSeparate = {
+    println {
+      (1 to 100)
+        .map {
+        case num if num % 15 == 0 => "FizzBuzz"
+        case num if num % 3 == 0 => "Fizz"
+        case num if num % 5 == 0 => "Buzz"
+        case num => num.toString
+      }
+        .mkString(",")
+    }
+  }
+
+  // Sum
+  def fizzBuzzNumSum = {
+    println {
+      (1 to 100)
+        .map {
+          case num if num % 15 == 0 => "FizzBuzz"
+          case num if num % 3 == 0 => "Fizz"
+          case num if num % 5 == 0 => "Buzz"
+          case num => num.toString
+        }
+        .collect {
+          case str if Try(str.toInt).isSuccess => str.toInt
+        }
+        .sum
+    }
+  }
 }
+
