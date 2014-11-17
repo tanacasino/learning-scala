5831b2870c272bb2ad52778bb491b03ed763c494
Tomofumi Tanaka <tanacasino@gmail.com>
2014-11-17 10:59:15 +0900

    Add NNP10 sample answer

--- src/main/scala/com/github/tanacasino/nnp/NNP10.scala
+++ src/main/scala/com/github/tanacasino/nnp/NNP10.scala
@@ -5,32 +5,35 @@ trait NNP10 {
 
   // P01 (*) Find the last element of a list.
   def last(list: List[Int]): Int = {
-    ???
+    list.last
   }
 
   // P02 (*) Find the last but one element of a list.
   def penultimate(list: List[Int]): Int = {
-    ???
+    list.takeRight(2).head  // 最後の２個をとって頭の要素
+    list.init.last          // head : tail == init last
+    list(list.length - 2)   //
   }
 
   def nth(n: Int, list: List[Int]): Int = {
-    ???
+    list(n)
   }
 
   def length(list: List[Int]): Int = {
-    ???
+    list.length
   }
 
   def reverse(list: List[Int]): List[Int] = {
-    ???
+    list.reverse
   }
 
   def isPalindrome(list: List[Int]): Boolean = {
-    ???
+    list == list.reverse
   }
 
   def flatten(nested: List[Any]): List[Any] = {
-    ???
+    // うーん難しいですねー
+    nested.flatten
   }
 
   def compress(list: List[Symbol]): List[Symbol] = {
