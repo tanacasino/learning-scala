2af06de8b898b5a171a731e33003ee7e3799db31
Takahiro Hagino <haginum@Takahiros-MacBook-Air.local>
2014-12-17 10:16:31 +0900

    Daily Scala Request

--- src/main/scala/com/github/takahirohagino/option/Request.scala
+++ src/main/scala/com/github/takahirohagino/option/Request.scala
@@ -5,17 +5,18 @@ package com.github.takahirohagino.option
  */
 case class Request (val params: Option[Map[String, String]]) {
 
-  val default: Map[String, String] = Map("start" -> "0", "end" -> "0")
-  val start = params.getOrElse(default).getOrElse("start", "10").toInt
-  val end = params.getOrElse(default).getOrElse("end", "10").toInt
+  private val default: Map[String, String] = Map("start" -> "0", "end" -> "0")
+  private val start = params.getOrElse(default).getOrElse("start", "10").toInt
+  private val end = params.getOrElse(default).getOrElse("end", "10").toInt
   val result = if (start > end) Some(end - start) else None
-
-  def getResult = result;
 
 }
 
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println(Request(Some(Map("start" -> "10", "end" -> "2"))).getResult);
+    println(difference(Request(Some(Map("start" -> "10", "end" -> "2")))));
   }
+
+  def difference(req: Request): Option[Int] = req.result
 }
